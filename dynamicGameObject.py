f52f54e6700de2b59f9c7e60e4337ef09837133b
AlyssonGGS <alyssonggs@hotmail.com>
2015-06-02 17:35:41 -0300

    mudança de pasta

--- dynamicGameObject.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'AlyssonNote'
-from PPlay.gameobject import *
-class DynamicGameObject(GameObject):
-    def _init_(self):
-        GameObject.__init__(self)
-        self.gravity = 100
-        return
